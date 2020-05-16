2a6a988508dcfd9481c5742599519b4d489f7fc8
thomasklz <pipos_rgt@hotmail.com>
2020-05-15 22:49:16 -0500

    parroquia cambio de diseño y codificación

--- src/app/components/parroquia/parroquia.component.ts
+++ src/app/components/parroquia/parroquia.component.ts
@@ -1,225 +1,172 @@
 import { Component, OnInit, ViewChild, ElementRef } from '@angular/core';
 import { FormGroup, FormControl, Validators, Form } from '@angular/forms';
-import { PanelAdministracionService } from 'src/app/services/panel-administracion.service';
-import { PersonaService } from 'src/app/services/persona.service';
-import { Parroquia } from 'src/app/interfaces/parroquia/parroquia';
-import { Canton } from 'src/app/interfaces/canton/canton';
-import sweetalert from 'sweetalert';
 import { LugaresService } from 'src/app/services/lugares.service';
 import { CantonComponent } from '../canton/canton.component';
 import { MatTable, MatDialog, MatSnackBar } from '@angular/material';
 import { ModalLugarRepresentanteComponent } from '../modal-lugar-representante/modal-lugar-representante.component';
-
-
+import { Observable } from 'rxjs';
+import { startWith, map } from 'rxjs/operators';
+import { MatAutocompleteSelectedEvent } from '@angular/material';
+import { MatPaginator } from '@angular/material/paginator';
+import { MatTableDataSource } from '@angular/material/table';
+import { MatSort } from '@angular/material/sort';
 @Component({
   selector: 'app-parroquia',
   templateUrl: './parroquia.component.html',
   styleUrls: ['./parroquia.component.css']
 })
 export class ParroquiaComponent implements OnInit {
-
-  
-
+  formParroquia: FormGroup;
+  filterCanton: string;
+  filteredCanton: Observable<string[]>;
+  control = new FormControl();
+  cantones: string[] = [];
+  _listaCantones:any[]=[];
+  _listaParroquias:any[]=[];
+  _nombreProvincia:string;
+  _nombreCanton:string;
+  _idCantonEncriptado:string;
+  _idProvinciaEncriptado:string;
+  tablaParroquias = ['codigo','parroquia', 'canton', 'acciones'];
+  _btnAccion="Guardar";
+  dataSource = new MatTableDataSource();
+  @ViewChild(MatPaginator, { static: true }) paginator: MatPaginator;
+  @ViewChild(MatSort, { static: true }) sort: MatSort;
   constructor(
     private lugaresService:LugaresService,
     private modalLugarRepresentante:MatDialog
     ,private snackBarComponent:MatSnackBar
   ) {
-
+    this.formParroquia = new FormGroup({
+      _descripcionParroquia: new FormControl(''),
+      _idParroquia:new FormControl(''),
+      _nombreParroquia: new FormControl('', [Validators.required]),
+      _codigoParroquia: new FormControl('', [Validators.required]),
+      _rutaLogoParroquia:new FormControl(''),
+      
+    });
   }
-
   ngOnInit() {
     this._consultarParroquias();
     this._consultarCantones();
+    this.filtroCantones();
+    this.dataSource.paginator = this.paginator;
+    this.dataSource.sort = this.sort;
   }
-
-
-  mensaje(_mensaje:string,_duracion?:number,_opcion?:number,_color?:string){
-
-    
-    if (_duracion==null) {
-       _duracion=3000;
-    }
-    if (_opcion==1) {
-      _mensaje="Datos ingresados correctamente";
-    }
-    if (_opcion==2) {
-      _mensaje="Datos modificados correctamente";
-    }
-    if (_opcion==3) {
-      _mensaje="Datos eliminados correctamente";
-    }
-    if (_color==null) {
-      _color ="gpm-danger";
-    }
-    let snackBarRef = this.snackBarComponent.open(_mensaje,null,{duration:_duracion,panelClass:['text-white',`${_color}`],data:{}});
+  applyFilter(event: Event) {
+    const filterValue = (event.target as HTMLInputElement).value;
+    this.dataSource.filter = filterValue.trim().toLowerCase();
   }
-
-  tablaParroquias = ['codigo','parroquia', 'canton', 'acciones'];
-  tablaCantones = ['canton', 'acciones'];
-
-  _idCantonEncriptado=""; _nombreCanton=""; _listaCantones:any[]=[]; _idProvinciaEncriptado=""; _nombreProvincia="";// la Canton que se escoje
-
-  _validar=true;
-  _idParroquiaEncriptado="";
-  _codigoParroquia="";
-  _nombreParroquia="";
-  _descripcionParroquia="";
-  _rutaLogoParroquia="";
-
-  _btnAccion="Guardar";
-
-  @ViewChild('frmParroquia',{static:false}) frmParroquia:Form;
-  @ViewChild(CantonComponent,{static:false}) CantonComponent: CantonComponent;
-
-  _limpiarForm(){
-    //----------Provincia-------------
-    this._idProvinciaEncriptado ="";
-    this._nombreProvincia ="";
-    //----------Canton-------------
-    this._idCantonEncriptado = "";
-    this._nombreCanton       = "";
-    //------------------------------
-    this._idParroquiaEncriptado="";
-    this._codigoParroquia="";
-    this._nombreParroquia="";
-    this._descripcionParroquia="";
-    this._rutaLogoParroquia="";
-
-    this._btnAccion = "Guardar";
-    this._validar = true;
+  filtroCantones() {
+    this.filteredCanton = this.control.valueChanges.pipe(
+      startWith(''),
+      map(value => value.length >= 1 ? this._filter(value) : [])
+    );
   }
-
-  _validarCompletos(event){
-    if (event.target.value==="") {
-      event.target.classList.add("is-invalid");
-    }else{
-      event.target.classList.remove("is-invalid");
-    }
-
-    if (
-      this._codigoParroquia      !="" &&
-      this._nombreParroquia      !="" &&
-      this._nombreCanton   !=""
-      // this._descripcionParroquia !="" &&
-      // this._rutaLogoParroquia    !=""
-    ) {
-      this._validar=false;
-    }else{
-      this._validar=true;
-    }
-
+  private _filter(value: string): string[] {
+    const filterValue = this._normalizeValue(value);
+    return this.cantones.filter(canton => this._normalizeValue(canton).includes(filterValue));
   }
-
-  _validarBoton(){
-    if (
-      this._codigoParroquia      !="" &&
-      this._nombreParroquia      !="" &&
-      this._nombreCanton         !="" &&
-      this._nombreProvincia      !=""
-      // this._descripcionParroquia !="" &&
-      // this._rutaLogoParroquia    !=""
-    ) {
-      this._validar=false;
-    }else{
-      this._validar=true;
-    }
+  private _normalizeValue(value: string): string {
+    return value.toLowerCase().replace(/\s/g, '');
   }
-
-  _validarFormulario(){
-    if (
-      this._codigoParroquia      !="" &&
-      this._nombreParroquia
-      // this._descripcionParroquia !="" &&
-      // this._rutaLogoParroquia    !=""
-    ) {
-      if (this._validar===false) {
+  onSelectionChanged(event: MatAutocompleteSelectedEvent, nombre: any) {
+    this._listaCantones.forEach(element => {
+      if (element.NombreCanton == nombre) {
+          this._nombreProvincia=element.Provincia.NombreProvincia;
+          this._nombreCanton=element.NombreCanton;
+          this._idCantonEncriptado=element.IdCantonEncriptado;
+          this._idProvinciaEncriptado=element.Provincia.IdProvinciaEncriptado;
+      }
+    });
+  }
+  mensaje(_mensaje: string, _duracion?: number, _color?: string) {
+    if (_duracion == null) {
+      _duracion = 3000;
+    }
+    if (_color == null) {
+      _color = "gpm-danger";
+    }
+    let snackBarRef = this.snackBarComponent.open(_mensaje, null, { duration: _duracion, panelClass: [`${_color}`], verticalPosition: 'bottom', horizontalPosition: 'end' });
+  }
+ _validarFormulario(){
         if (this._btnAccion==="Guardar") {
           this._ingresarParroquia();
         }else if (this._btnAccion==="Modificar") {
           this._modificarParroquia();
         }
-      }
-
-    }
   }
-
-  _listaParroquias:any[]=[];
   _consultarParroquias(){
-    //this.native_codigoParroquia.nativeElement.value;
+    this._listaParroquias = null;
     this.lugaresService._consultarParroquias()
       .then(data=>{
         if (data['http']['codigo']=='200') {
           this._listaParroquias=data['respuesta'];
-          console.log(this._listaParroquias);
-
-        }else{
-          console.log(data['http']);
+          this.dataSource.data = this._listaParroquias;
         }
       })
       .catch(error=>{
-        console.log(error);
-      }).finally(()=>{
-
-      });
+        this.mensaje(error);
+      })
   }
-
+  clearMenssageError(){
+  this.formParroquia.controls['_nombreParroquia'].setErrors(null);
+  this.formParroquia.controls['_codigoParroquia'].setErrors(null);
+  this.formParroquia.controls['_descripcionParroquia'].setErrors(null);
+ }
   _ingresarParroquia(){
-
     this.lugaresService._insertarParroquia(
-      this._codigoParroquia,
-      this._nombreParroquia,
-      this._descripcionParroquia,
-      this._rutaLogoParroquia,
+      this.formParroquia.value._codigoParroquia,
+      this.formParroquia.value._nombreParroquia,
+      this.formParroquia.value._descripcionParroquia,
+      this.formParroquia.value._rutaLogoParroquia,
       this._idCantonEncriptado,
       this._idProvinciaEncriptado,
     ).then(data=>{
       if (data['http']['codigo']=='200') {
         this._consultarParroquias();
-        this._consultarCantones();
-        this._limpiarForm();
-        this._validar=true;
-        this._validarBoton();
-        // this._validarFormulario();
+        this.formParroquia.reset();
+        this.clearMenssageError();
+        this.formParroquia.setErrors({ invalid: true });
+        this.mensaje("Parroquia registrada", null, 'msj-success');
       }else if (data['http']['codigo']=='500') {
         this.mensaje("A ocurrido un error inesperado, intente más tarde.")
       }else{
-        console.log(data['http']);
         this.mensaje(data['http']['mensaje']);
       }
-    }).catch(error=>{
-
-    }).finally(()=>{
-
-    });
+    })
   }
   _modificarParroquia(){
+    if(this.formParroquia.value._descripcionParroquia=="null"){
+      this.formParroquia.get('_descripcionParroquia').setValue("");
+    }
     this.lugaresService._modificarParroquia(
-      this._idParroquiaEncriptado,
-      this._codigoParroquia,
-      this._nombreParroquia,
-      this._descripcionParroquia,
-      this._rutaLogoParroquia,
+      this.formParroquia.value._idParroquia,
+      this.formParroquia.value._nombreParroquia,
+      this.formParroquia.value._codigoParroquia,
+      this.formParroquia.value._descripcionParroquia,
+      this.formParroquia.value._rutaLogoParroquia,
       this._idCantonEncriptado,
-      this._idProvinciaEncriptado
+      this._idProvinciaEncriptado      
     ).then(data=>{
-      // console.log(data);
-      
       if (data['http']['codigo']=='200') {
         this._consultarParroquias();
-        this._consultarCantones();
-        this._limpiarForm();
+        this.formParroquia.reset();
+        this._btnAccion = "Guardar";
+        this.clearMenssageError();
+        this.formParroquia.setErrors({ invalid: true });
+        this._idProvinciaEncriptado="";
+        this._nombreProvincia="";
+        this._idCantonEncriptado="";
+        this._nombreCanton="";
+        this.mensaje("Registro actualizado", null, 'msj-success');
       }else if (data['http']['codigo']=='500') {
         this.mensaje("A ocurrido un error inesperado, intente más tarde.")
       }else{
         console.log(data['http']);
         this.mensaje(data['http']['mensaje']);
       }
-    }).catch(error=>{
-
-    }).finally(()=>{
-
-    });
+    })
   }
   _eliminarParroquia(_item){
     this.lugaresService._eliminarParroquia(
@@ -227,107 +174,65 @@ export class ParroquiaComponent implements OnInit {
     ).then(data=>{
       if (data['http']['codigo']=='200') {
         this._consultarParroquias();
-        this._consultarCantones();
+        this.mensaje("Registro eliminado", null, 'msj-success');
       }else if (data['http']['codigo']=='500') {
         this.mensaje("A ocurrido un error inesperado, intente más tarde.")
       }else{
-        console.log(data['http']);
         this.mensaje(data['http']['mensaje']);
       }
-    }).catch(error=>{
-
-    }).finally(()=>{
-
-    });
+    })
   }
-  _CantonQuitada:any="";
   _prepararParroquia(_item){
-    //------------------Provincia--------------------------------------
     this._idProvinciaEncriptado = _item.Canton.Provincia.IdProvinciaEncriptado;
     this._nombreProvincia       = _item.Canton.Provincia.NombreProvincia;
-    //------------------Canton----------------------------------------
     this._idCantonEncriptado = _item.Canton.IdCantonEncriptado;
     this._nombreCanton       = _item.Canton.NombreCanton;
-    //---------------------------------------------------------
-    this._idParroquiaEncriptado =_item.IdParroquiaEncriptado;
-    this._codigoParroquia       =_item.CodigoParroquia;
-    this._nombreParroquia       =_item.NombreParroquia;
-    
-    this._descripcionParroquia  =_item.DescripcionParroquia;
-    if (this._descripcionParroquia=='null') {
-      this._descripcionParroquia="";
+    this.formParroquia.get('_idParroquia').setValue(_item.IdParroquiaEncriptado);
+    this.formParroquia.get('_nombreParroquia').setValue(_item.CodigoParroquia);
+    this.formParroquia.get('_codigoParroquia').setValue(_item.NombreParroquia);
+    if(_item.DescripcionParroquia=="null"){
+      this.formParroquia.get('_descripcionParroquia').setValue(null);
+    }else{
+      this.formParroquia.get('_descripcionParroquia').setValue(_item.DescripcionParroquia);
     }
-    this._rutaLogoParroquia     =_item.RutaLogoParroquia;
+    this.formParroquia.get('_rutaLogoParroquia').setValue(_item.RutaLogoParroquia);
     this._btnAccion = "Modificar";
-    this._validarBoton();
   }
-  @ViewChild('MatTableCantones',{static:false}) MatTableCantones :MatTable<any>;
-  _prepararCanton(_item){
-    //------------------Provincia--------------------------------------
-    this._idProvinciaEncriptado = _item.Provincia.IdProvinciaEncriptado;
-    this._nombreProvincia       = _item.Provincia.NombreProvincia;
-    //------------------Canton----------------------------------------
-    this._idCantonEncriptado =_item.IdCantonEncriptado;
-    this._nombreCanton       =_item.NombreCanton;
-
-    // this._idProvinciaEncriptado = _item.Provincia.IdProvinciaEncriptado;
-
-    if (this._CantonQuitada!="") {
-      // this._consultarCantones();
-      this._listaCantones.push(this._CantonQuitada);
-
-    }
-    
-    var obj = this._listaCantones.find(dato=>dato.IdCantonEncriptado==_item.IdCantonEncriptado);
-    console.log(obj);
-    
-    var index = this._listaCantones.indexOf(obj);
-    console.log(index);
-    
-    this._listaCantones.splice(index,1);
-    this._CantonQuitada = _item;
-
-    this.MatTableCantones.dataSource = this._listaCantones;
-    // this._listaCantones.sort();
-    this.MatTableCantones.renderRows();
-
-  }
-
   _consultarCantones(){
-    //this.native_codigoCanton.nativeElement.value;
     this.lugaresService._consultarCantones()
       .then(data=>{
         if (data['http']['codigo']=='200') {
           this._listaCantones=data['respuesta'];
+          this.cantones.length = 0;
+          for (let index = 0; index < this._listaCantones.length; index++) {
+            this.cantones.push(this._listaCantones[index].NombreCanton);
+          }
+          this.filtroCantones();
         }else{
-          console.log(data['http']);
+         this.mensaje(data['http']);
         }
       })
       .catch(error=>{
-        console.log(error);
-      }).finally(()=>{
-        // this._listaCantones.sort();
-      });
+        this.mensaje(error);
+      })
   }
-
   _verRepresentante(_item){
     let dialogRef = this.modalLugarRepresentante.open(ModalLugarRepresentanteComponent, {
       width: 'auto',
       height: 'auto',
       data: { lugar_tipo: 'parroquia', lugar_data: _item }
     });
-    dialogRef.afterClosed().subscribe(result=>{
-      // console.log(result);
-      if (result) {
-        
-      }
-    },error=>{
-
-    },()=>{
-      this._consultarParroquias();
-    }); 
   }
-
+  _limpiarForm(){
+    this.formParroquia.reset();
+    this._btnAccion = "Guardar";
+    this.clearMenssageError();
+    this.formParroquia.setErrors({ invalid: true });
+    this._idProvinciaEncriptado="";
+    this._nombreProvincia="";
+    this._idCantonEncriptado="";
+    this._nombreCanton="";
+  }
 }
 
 
